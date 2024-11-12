b3292378b90f0defe4c3d3a70d44cb50941078ee
Wesleyn77 <164032571+Wesleyn77@users.noreply.github.com>
2024-11-12 10:27:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem ganhou a copa do brasil',
+    'Flamengo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Ingles',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
